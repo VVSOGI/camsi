14fa9598ee79e4cd11d852bbe3198f87c86948ca
benny <vvsogi@gmail.com>
2025-05-24 01:38:17 +0900

    Refactor draw method in Drag class to improve shape rendering

--- 004/drag.js
+++ 004/drag.js
@@ -20,12 +20,32 @@ export class Drag {
   };
 
   draw = (ctx, mousePosition) => {
+    if (!this.tempPoint) return;
+
     const { mouseX, mouseY } = mousePosition;
 
     ctx.beginPath();
     ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(this.tempPoint.x, mouseY);
     ctx.lineTo(this.tempPoint.x, this.tempPoint.y);
+    ctx.lineTo(mouseX, this.tempPoint.y);
+    ctx.lineTo(mouseX, mouseY);
+    ctx.fill();
+    ctx.fillStyle = "rgba(105, 105, 230, 0.1)";
+    ctx.strokeStyle = "rgba(105, 105, 230)";
     ctx.stroke();
     ctx.closePath();
+
+    const left = Math.min(mouseX, this.tempPoint.x);
+    const top = Math.min(mouseY, this.tempPoint.y);
+    const right = Math.max(mouseX, this.tempPoint.x);
+    const bottom = Math.max(mouseY, this.tempPoint.y);
+
+    return {
+      x1: left,
+      y1: top,
+      x2: right,
+      y2: bottom,
+    };
   };
 }
