34e335c5927700e6a73431e756811cc394fcced5
benny <vvsogi@gmail.com>
2025-05-26 16:53:15 +0900

    Refactor Line class methods and add threshold for distance calculation

--- 004/line.js
+++ 004/line.js
@@ -10,43 +10,8 @@ export class Line {
     this.gap = 10;
     this.dragCornerRectSize = 10;
 
-    this.points = [];
-    this.intervals = 30;
+    this.threshold = 10;
   }
-
-  updatePoints = () => {
-    for (let i = 0; i <= this.intervals; i++) {
-      const ratio = i / this.intervals;
-      const x = this.x1 + (this.x2 - this.x1) * ratio;
-      const y = this.y1 + (this.y2 - this.y1) * ratio;
-      this.points.push({ x, y });
-    }
-  };
-
-  isInSquare = (mousePosition) => {
-    const { mouseX, mouseY } = mousePosition;
-
-    if (mouseX >= this.x1 && mouseX <= this.x2 && mouseY >= this.y1 && mouseY <= this.y2) {
-      return true;
-    }
-
-    return false;
-  };
-
-  isMouseOnLine = (mousePosition) => {
-    const { mouseX, mouseY } = mousePosition;
-    let begin = this.points[0];
-
-    for (let i = 1; i < this.points.length; i++) {
-      const next = this.points[i];
-      if (mouseX >= begin.x && mouseX <= next.x && mouseY >= begin.y && mouseY <= next.y) {
-        return true;
-      }
-      begin = next;
-    }
-
-    return false;
-  };
 
   onHover = (mousePosition) => {
     const isNeedCalculate = this.isInSquare(mousePosition);
@@ -55,13 +20,53 @@ export class Line {
       return false;
     }
 
-    const isHover = this.isMouseOnLine(mousePosition);
-
-    if (!isHover) {
+    const distance = this.getDistanceFromLine(mousePosition);
+    if (distance > this.threshold) {
       return false;
     }
 
     return true;
+  };
+
+  onClick = (mousePosition) => {
+    const isNeedCalculate = this.isInSquare(mousePosition);
+
+    if (!isNeedCalculate) {
+      return false;
+    }
+
+    const distance = this.getDistanceFromLine(mousePosition);
+    if (distance <= this.threshold) {
+      this.drag = true;
+    }
+  };
+
+  isInSquare = (mousePosition) => {
+    const { mouseX, mouseY } = mousePosition;
+
+    const left = Math.min(this.x1, this.x2);
+    const right = Math.max(this.x1, this.x2);
+    const top = Math.min(this.y1, this.y2);
+    const bottom = Math.max(this.y1, this.y2);
+
+    if (mouseX >= left && mouseX <= right && mouseY >= top && mouseY <= bottom) {
+      return true;
+    }
+
+    return false;
+  };
+
+  /**
+   * 직선의 방정식을 이용해서 마우스의 x, y 값이 직선으로부터 얼마나 떨어져있는지 확인
+   */
+  getDistanceFromLine = (mousePosition) => {
+    const { mouseX, mouseY } = mousePosition;
+
+    const A = this.y2 - this.y1;
+    const B = this.x1 - this.x2;
+    const C = this.x2 * this.y1 - this.x1 * this.y2;
+
+    return Math.abs(A * mouseX + B * mouseY + C) / Math.sqrt(A * A + B * B);
   };
 
   openDragRange = (ctx) => {
@@ -106,30 +111,6 @@ export class Line {
     ctx.strokeStyle = "black";
     ctx.stroke();
     ctx.closePath();
-
-    for (const point of this.points) {
-      this.drawDot(ctx, point.x, point.y);
-    }
-
-    let begin = this.points[0];
-    ctx.save();
-    ctx.beginPath();
-    ctx.moveTo(begin.x, begin.y);
-
-    for (let i = 1; i < this.points.length; i++) {
-      const current = this.points[i];
-      ctx.lineTo(current.x, begin.y);
-      ctx.lineTo(current.x, current.y);
-      ctx.lineTo(begin.x, current.y);
-      ctx.lineTo(begin.x, begin.y);
-      ctx.moveTo(current.x, current.y);
-      begin = current;
-    }
-
-    ctx.fillStyle = "rgba(105, 105, 230, 0.5)";
-    ctx.fill();
-    ctx.closePath();
-    ctx.restore();
 
     if (this.drag) {
       this.openDragRange(ctx);
